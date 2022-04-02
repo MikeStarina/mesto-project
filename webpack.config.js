24442c2a0211ca16567d083a3021228cb078ce37
Mike Starina <x@pnhd.ru>
2022-04-02 21:47:26 +0300

    ready 4 review 1

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,10 @@ module.exports = {
   },
   mode: 'development',
   devServer: {
-    contentBase: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, './dist'),
     open: true,
     compress: true,
-    port: 8080
+    port: 8081
   },
   module: {
     rules: [{
